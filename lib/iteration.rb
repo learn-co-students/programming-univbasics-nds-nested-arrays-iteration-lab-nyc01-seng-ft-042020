5dfb01feb06c5387c0c08d03c2d32302cac8bf00
Cody DeMartin <codythomasdemartin@gmail.com>
2020-03-23 22:37:16 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,12 +6,31 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  joined = [ ]
+  row_index = 0 
+  while row_index < src.count do
+      joined << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+    row_index += 1
+  end
+  joined
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  greater = [ ]
+  pairs_index = 0 
+  while pairs_index < src.count do
+    if src[pairs_index][0] < src[pairs_index][1]
+      greater << src[pairs_index][1]
+      pairs_index += 1
+    else
+      greater << src[pairs_index][0]
+      pairs_index +=1 
+    end
+  end  
+  greater
 end
 
 def total_even_pairs(src)
@@ -22,4 +41,15 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  total = 0 
+  number_pairs_traversed = 0 
+  while number_pairs_traversed < src.count do 
+    if number_pairs_traversed[0] %2 == 0 && number_pairs_traversed[1] %2 == 0
+      total += number_pairs_traversed[0] + number_pairs_traversed[1]
+      number_pairs_traversed += 1         
+    else
+      number_pairs_traversed += 1  
+    end
+  end
+  total
 end
